9c3d58cb1769b3d0fdaf14f843a1dd35eccd8464
destroyedlolo <l_faillie@yahoo.com>
2015-04-26 22:56:21 +0200

    SetEncoding implemented

--- src/directfb.c
+++ src/directfb.c
@@ -167,14 +167,14 @@ static int FontStringWidth(lua_State *L){
 }
 
 static DFBEnumerationResult enccallback(DFBTextEncodingID id, const char *n, void *data){
-	lua_pushinteger( ((struct callbackContext *)data)->L, ++((struct callbackContext *)data)->index );
+	lua_pushinteger( ((struct callbackContext *)data)->L, id );
 	lua_newtable(((struct callbackContext *)data)->L);
 
 	lua_pushstring( ((struct callbackContext *)data)->L, "id");
 	lua_pushinteger( ((struct callbackContext *)data)->L, id );
 	lua_settable( ((struct callbackContext *)data)->L, 4 );
 
-	lua_pushstring( ((struct callbackContext *)data)->L, "id");
+	lua_pushstring( ((struct callbackContext *)data)->L, "name");
 	lua_pushstring( ((struct callbackContext *)data)->L, n );
 	lua_settable( ((struct callbackContext *)data)->L, 4 );
 
@@ -189,11 +189,74 @@ static int EnumEncodings(lua_State *L){
 	dt.L = L;
 	dt.index = 0;
 
+	if(!font){
+		lua_pushnil(L);
+		lua_pushstring(L, "EnumEncodings() with a dead font");
+		return 2;
+	}
+
 	lua_newtable(L);	/* Result table */
 
 	font->EnumEncodings(font, enccallback, &dt);
 
 	return 1;
+}
+
+static int FindEncoding(lua_State *L){
+	IDirectFBFont *font = *checkSelFont(L);
+	const char *encname = luaL_checkstring(L, 2);
+	DFBResult err;
+	DFBTextEncodingID id;
+
+	if(!font){
+		lua_pushnil(L);
+		lua_pushstring(L, "FindEncoding() with a dead font");
+		return 2;
+	}
+
+	if((err = font->FindEncoding(font, encname, &id)) != DFB_OK){
+		lua_pushnil(L);
+		lua_pushstring(L, DirectFBErrorString(err));
+		return 2;
+	}
+	lua_pushinteger(L, id);
+
+	return 1;
+}
+
+static int SetEncoding(lua_State *L){
+	IDirectFBFont *font = *checkSelFont(L);
+	DFBTextEncodingID eid;
+	DFBResult err;
+
+	if(!font){
+		lua_pushnil(L);
+		lua_pushstring(L, "FindEncoding() with a dead font");
+		return 2;
+	}
+
+	if(lua_isstring(L, 2)){
+		const char *encname = lua_tostring(L, 2);
+		if((err = font->FindEncoding(font, encname, &eid)) != DFB_OK){
+			lua_pushnil(L);
+			lua_pushstring(L, DirectFBErrorString(err));
+			return 2;
+		}
+	} else if(lua_isnumber(L, 2))
+		eid = lua_tointeger(L,2);
+	else {
+		lua_pushnil(L);
+		lua_pushstring(L, "SetEncoding() requires a number or a string");
+		return 2;
+	}
+
+	if((err = font->SetEncoding(font, eid)) != DFB_OK){
+		lua_pushnil(L);
+		lua_pushstring(L, DirectFBErrorString(err));
+		return 2;
+	}
+
+	return 0;
 }
 
 static const struct luaL_reg SelFontLib [] = {
@@ -206,6 +269,8 @@ static const struct luaL_reg SelFontM [] = {
 	{"destroy", FontRelease},	/* Alias */
 	{"GetHeight", FontGetHeight},
 	{"EnumEncodings", EnumEncodings},
+	{"FindEncoding", FindEncoding},
+	{"SetEncoding", SetEncoding},
 	{"StringWidth", FontStringWidth},
 	{NULL, NULL}
 };
