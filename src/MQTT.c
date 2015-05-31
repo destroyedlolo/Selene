7fff70b17a2916a48add11290d0cd3d690278778
destroyedlolo <l_faillie@yahoo.com>
2015-05-31 19:10:16 +0000

    MQTT_connect() arguments handling

--- src/MQTT.c
+++ src/MQTT.c
@@ -10,6 +10,10 @@
 #include <assert.h>
 
 static int smq_connect(lua_State *L){
+/* Connect to a broker
+ * 1 : broker's host
+ * 2 : table of arguments
+ */
 	MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
 	const char *host = luaL_checkstring(L, 1);	/* Host to connect to */
 
@@ -19,6 +23,67 @@ static int smq_connect(lua_State *L){
 		return 2;
 	}
 
+	lua_pushstring(L, "keepAliveInterval");
+	lua_gettable(L, -2);
+	if( lua_type(L, -1) == LUA_TNUMBER )
+		conn_opts.keepAliveInterval = lua_tointeger(L, -1);
+	lua_pop(L, 1);	/* cleaning ... */
+
+	lua_pushstring(L, "cleansession");
+	lua_gettable(L, -2);
+	switch( lua_type(L, -1) ){
+	case LUA_TBOOLEAN:
+		conn_opts.cleansession = lua_toboolean(L, -1) ? 1 : 0;
+		break;
+	case LUA_TNUMBER:
+		conn_opts.cleansession = lua_tointeger(L, -1) ? 1 : 0;
+		break;
+	case LUA_TNIL:
+		conn_opts.cleansession = 0;
+		break;
+	default :
+		lua_pushnil(L);
+		lua_pushstring(L, lua_typename(L, lua_type(L, -2) ));
+		lua_pushstring(L," : don't know how to convert to boolean");
+		lua_concat(L, 2);
+		return 2;
+
+	}
+	lua_pop(L, 1);	/* cleaning ... */
+
+	lua_pushstring(L, "reliable");
+	lua_gettable(L, -2);
+	switch( lua_type(L, -1) ){
+	case LUA_TBOOLEAN:
+		conn_opts.reliable = lua_toboolean(L, -1) ? 1 : 0;
+		break;
+	case LUA_TNUMBER:
+		conn_opts.reliable = lua_tointeger(L, -1) ? 1 : 0;
+		break;
+	case LUA_TNIL:
+		conn_opts.reliable = 0;
+		break;
+	default :
+		lua_pushnil(L);
+		lua_pushstring(L, lua_typename(L, lua_type(L, -2) ));
+		lua_pushstring(L," : don't know how to convert to boolean");
+		lua_concat(L, 2);
+		return 2;
+
+	}
+	lua_pop(L, 1);	/* cleaning ... */
+
+	lua_pushstring(L, "username");
+	lua_gettable(L, -2);
+	if( lua_type(L, -1) == LUA_TSTRING )
+		conn_opts.username = lua_tostring(L, -1);
+	lua_pop(L, 1);	/* cleaning ... */
+
+	lua_pushstring(L, "password");
+	lua_gettable(L, -2);
+	if( lua_type(L, -1) == LUA_TSTRING )
+		conn_opts.password = lua_tostring(L, -1);
+	lua_pop(L, 1);	/* cleaning ... */
 
 	return 0;
 }
