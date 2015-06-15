ff31632eb56a39da7b03fb44f90e3060bb6ce5c6
destroyedlolo <l_faillie@yahoo.com>
2015-06-15 23:04:56 +0200

    Shared variables implemented

--- src/sharedobj.c
+++ src/sharedobj.c
@@ -11,6 +11,9 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define SO_VAR_LOCK 1
+#define SO_NO_VAR_LOCK 0
+
 static struct {
 	struct SharedVar *first, *last;
 	pthread_mutex_t mutex;	/*AF* As long their is only 2 threads, a simple mutex is enough */
@@ -23,12 +26,18 @@ static int crc( const char *s ){
 	return r;
 }
 
-static struct SharedVar *findvar(const char *vn){
+static struct SharedVar *findvar(const char *vn, int lock){
+/* Find a variable
+ * vn -> Variable name
+ * lock -> lock (!=0) or not the variable
+ */
 	int acrc = crc(vn);	/* get the crc of the variable name */
 
 	pthread_mutex_lock( &GlobalVarList.mutex );
 	for(struct SharedVar *v = GlobalVarList.first; v; v=v->succ){
 		if(v->crc == acrc && !strcmp(v->name, vn)){
+			if(lock)
+				pthread_mutex_lock( &v->mutex );
 			pthread_mutex_unlock( &GlobalVarList.mutex );
 			return v;
 		}
@@ -38,7 +47,7 @@ static struct SharedVar *findvar(const char *vn){
 }
 
 	/* Lua functions */
-static int ss_dump(lua_State *L){
+static int so_dump(lua_State *L){
 	pthread_mutex_lock( &GlobalVarList.mutex );
 
 	printf("List f:%p l:%p\n", GlobalVarList.first, GlobalVarList.last);
@@ -66,9 +75,9 @@ static int ss_dump(lua_State *L){
 	return 0;
 }
 
-static int ss_set(lua_State *L){
+static int so_set(lua_State *L){
 	const char *vname = luaL_checkstring(L, 1);	/* Name of the variable to retrieve */
-	struct SharedVar *v = findvar(vname);
+	struct SharedVar *v = findvar(vname, SO_VAR_LOCK);
 
 	if(v){	/* The variable already exists */
 		if(v->type == SOT_STRING && v->val.str)	/* Free previous allocation */
@@ -79,6 +88,7 @@ static int ss_set(lua_State *L){
 		v->crc = crc(vname);
 		v->type = SOT_UNKNOWN;
 		pthread_mutex_init(&v->mutex,NULL);
+		pthread_mutex_lock( &v->mutex );
 
 			/* Insert this new variable in the list */
 		pthread_mutex_lock( &GlobalVarList.mutex );
@@ -94,7 +104,6 @@ static int ss_set(lua_State *L){
 		pthread_mutex_unlock( &GlobalVarList.mutex );
 	}
 
-	pthread_mutex_lock( &v->mutex );
 	switch(lua_type(L, 2)){
 	case LUA_TSTRING:
 		v->type = SOT_STRING;
@@ -120,9 +129,31 @@ static int ss_set(lua_State *L){
 	return 0;
 }
 
+static int so_get(lua_State *L){
+	const char *vname = luaL_checkstring(L, 1);	/* Name of the variable to retrieve */
+	struct SharedVar *v = findvar(vname, SO_VAR_LOCK);
+	if(v){
+		switch(v->type){
+		case SOT_STRING:
+		case SOT_XSTRING:
+			lua_pushstring(L, v->val.str);
+			break;
+		case SOT_NUMBER:
+			lua_pushnumber(L, v->val.num);
+			break;
+		default :
+			lua_pushnil(L);
+			break;
+		}
+	}
+
+	return 1;
+}
+
 static const struct luaL_reg SelSharedLib [] = {
-	{"set", ss_set},
-	{"dump", ss_dump},
+	{"set", so_set},
+	{"get", so_get},
+	{"dump", so_dump},
 	{NULL, NULL}
 };
 
