43708325b558f813cc19cc64656f95caa7073665
destroyedlolo <l_faillie@yahoo.com>
2015-07-03 22:02:34 +0000

    Create Timer.h

---
+++ src/Timer.h
@@ -0,0 +1,13 @@
+/* Timer.h
+ *
+ * Everything to be shared for timers
+ *
+ * 03/07/2015 LF : First version
+ */
+
+#ifndef SELTIMER_H
+#define SELTIMER_H
+
+void init_SelTimer( lua_State * );
+
+#endif
