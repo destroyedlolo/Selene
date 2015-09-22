77a61dd7d6d84721315f6a0ab18c86d500dc11e6
destroyedlolo <l_faillie@yahoo.com>
2015-09-22 15:08:50 +0200

    Correct a mispealing

--- src/MQTT.c
+++ src/MQTT.c
@@ -264,7 +264,7 @@ static int smq_connect(lua_State *L){
 
 	if(!lua_istable(L, -1)){	/* Argument has to be a table */
 		lua_pushnil(L);
-		lua_pushstring(L, "SelSurface.create() is expecting a table");
+		lua_pushstring(L, "SelMQTT.create() is expecting a table");
 		return 2;
 	}
 
