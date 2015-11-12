be20e7339ed46a83b1c8409a97eeabef122f37bd
destroyedlolo <l_faillie@yahoo.com>
2015-11-13 00:06:54 +0100

    Once can be a SelShared's TaskOnceConst

--- src/Timer.c
+++ src/Timer.c
@@ -99,6 +99,8 @@ static int TimerCreate(lua_State *L){
 	lua_gettable(L, -2);
 	if( lua_type(L, -1) == LUA_TBOOLEAN )
 		task_once = lua_toboolean(L, -1);
+	else if( lua_type(L, -1) == LUA_TNUMBER )
+		task_once = lua_tointeger(L, -1);
 	lua_pop(L, 1);	/* Pop the value */
 
 #if 0
