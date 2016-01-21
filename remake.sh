91101131005bfafc97b2d49a584880329aff702e
destroyedlolo <l_faillie@yahoo.com>
2016-01-22 00:20:07 +0100

    Add dependancy on Makefile

--- remake.sh
+++ remake.sh
@@ -2,5 +2,5 @@
 
 cd src
 
-LFMakeMaker -v -cc='gcc -Wall -DUSE_DIRECTFB -DUSE_MQTT -DxDEBUG -std=c99 `directfb-config --cflags` `directfb-config --libs` -llua -lpaho-mqtt3c' *.c -t=../Selene > Makefile
+LFMakeMaker -v +f=Makefile -cc='gcc -Wall -DUSE_DIRECTFB -DUSE_MQTT -DxDEBUG -std=c99 `directfb-config --cflags` `directfb-config --libs` -llua -lpaho-mqtt3c' *.c -t=../Selene > Makefile
 
