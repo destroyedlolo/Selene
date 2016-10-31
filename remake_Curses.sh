e61388463aaa5fc7b25a8bf5b63a2ac734d8479f
destroyedlolo <l_faillie@yahoo.com>
2016-10-31 20:23:12 +0100

    Handle curses v5 and v6

--- remake_Curses.sh
+++ remake_Curses.sh
@@ -6,17 +6,37 @@
 # DEBUG - Add debuging messages
 # PLUGIN_DIR - where plugins .so can be found (default is /usr/local/lib/Selene)
 
+if which ncursesw6-config; then
+	echo "Found ncurses v6"
+	NCURSES=ncursesw6-config
+elif which ncursesw5-config; then
+	echo "Found ncurses v5"
+	NCURSES=ncursesw5-config
+else
+	echo "No ncurses found"
+	exit 20
+fi
+
 cd src
 
-LFMakeMaker -v +f=Makefile -cc='gcc -Wall -DUSE_CURSES -DUSE_MQTT -DPLUGIN_DIR=\".\" -DxDEBUG -std=c99 `ncursesw5-config --cflags` `ncursesw5-config --libs` `pkg-config --cflags lua` `pkg-config --libs lua` -lpaho-mqtt3c -ldl -Wl,--export-dynamic -lpthread' *.c -t=../Selene > Makefile
+echo
+echo "Main source"
+echo "-----------"
+LFMakeMaker -v +f=Makefile -cc='gcc -Wall -DUSE_CURSES -DUSE_MQTT -DPLUGIN_DIR=\".\" -DxDEBUG -std=c99 `'$NCURSES' --cflags` `'$NCURSES' --libs` `pkg-config --cflags lua` `pkg-config --libs lua` -lpaho-mqtt3c -ldl -Wl,--export-dynamic -lpthread' *.c -t=../Selene > Makefile
 
 cd SelPlugins/DirectFB
 
+echo
+echo "DirectFB source"
+echo "-----------"
 LFMakeMaker -v +f=Makefile -cc='gcc -Wall -fPIC -std=c99 ' *.c -so=../../../SelDirectFB.so > Makefile
 
 cd ../Curses
 
-LFMakeMaker -v +f=Makefile -cc='gcc -Wall -DUSE_CURSES `ncursesw5-config --cflags` -fPIC -std=c99 ' *.c -so=../../../SelCurses.so > Makefile
+echo
+echo "Curses source"
+echo "-----------"
+LFMakeMaker -v +f=Makefile -cc='gcc -Wall -DUSE_CURSES `'$NCURSES' --cflags` -fPIC -std=c99 ' *.c -so=../../../SelCurses.so > Makefile
 
 cd ../../..
 
