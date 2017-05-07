c77d769fb85d372f08587c2039b2dca0535d15a1
destroyedlolo <l_faillie@yahoo.com>
2017-05-08 00:44:33 +0200

    Correcting ... (oups)

--- remake_NoPlug.sh
+++ remake_NoPlug.sh
@@ -11,7 +11,7 @@ cd src
 echo
 echo "Main source"
 echo "-----------"
-LFMakeMaker -v +f=Makefile -cc='gcc -Wall -DUSE_CURSES -DUSE_MQTT -DPLUGIN_DIR=\"/usr/local/lib/Selene\" -DxDEBUG -std=c99 `'$NCURSES' --cflags` `'$NCURSES' --libs` `pkg-config --cflags lua` `pkg-config --libs lua` -lpaho-mqtt3c -ldl -Wl,--export-dynamic -lpthread' *.c -t=../Selene > Makefile
+LFMakeMaker -v +f=Makefile -cc='gcc -Wall -DUSE_CURSES -DUSE_MQTT -DPLUGIN_DIR=\"/usr/local/lib/Selene\" -DxDEBUG -std=c99 `pkg-config --cflags lua` `pkg-config --libs lua` -lpaho-mqtt3c -ldl -Wl,--export-dynamic -lpthread' *.c -t=../Selene > Makefile
 
 cd SelPlugins/DirectFB
 
@@ -25,7 +25,7 @@ cd ../Curses
 echo
 echo "Curses source"
 echo "-----------"
-LFMakeMaker -v +f=Makefile -cc='gcc -Wall --cflags` -fPIC -std=c99 ' *.c -so=../../../SelCurses.so > Makefile
+LFMakeMaker -v +f=Makefile -cc='gcc -Wall -fPIC -std=c99 ' *.c -so=../../../SelCurses.so > Makefile
 
 cd ../../..
 
