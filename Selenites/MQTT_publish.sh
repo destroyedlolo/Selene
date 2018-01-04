066076040c1f43b4faed44f43f1547d0fef70ae9
destroyedlolo <l_faillie@yahoo.com>
2018-01-04 22:15:43 +0100

    Connect to the broker

--- Selenites/MQTT_publish.sh
+++ Selenites/MQTT_publish.sh
@@ -9,11 +9,12 @@ local qos = 0;
 
 function help()
 	print "Publish a message to a specified MQTT topic"
-	print "-H,--host : MQTT server hostname (default localhost)"
+	print "-h,--host : MQTT server hostname (default localhost)"
 	print "-p,--port : MQTT server port number (default 1883)"
 	print "-t,--topic : Topic on which to publish"
 	print "-m,--message : Message to be published"
 	print "-q,--qos : qos to use (default 0)"
+	print "-v : be verbose"
 
 	os.exit(10)
 end
@@ -39,6 +40,12 @@ while i<=#arg do
 	elseif arg[i] == '-q' or arg[i] == '--qos' then
 		i = i + 1
 		qos = tonumber(arg[i])
+	elseif arg[i] == '-v' then
+		verbose = true
+	else
+		print("Unknown option '"..arg[i].."'")
+		print ""
+		help()
 	end
 	i = i+1
 end
@@ -47,5 +54,19 @@ if not topic or not message then
 	print "Missing topic or message"
 	print ""
 	help()
+end
+
+local url = "tcp://"..server..":"..port
+if verbose then
+	print("url=", url)
+end
+
+local brk,err = SelMQTT.connect( url, { reliable=false, clientID='SelPublish' } )
+if not brk then
+	print( err )
+	return
+end
+if verbose then
+	print "Connected ..."
 end
 
