bcd3f890137a77686624278a3195f183f4a0d9a6
destroyedlolo <l_faillie@yahoo.com>
2018-04-02 00:13:10 +0200

    Move to monolithyque library include

---
+++ src/SeleneLibrary/libSelene.h
@@ -0,0 +1,16 @@
+/*	libSelene.h
+ *
+ */
+
+#ifndef SEL_LIBRARY_H
+#define SEL_LIBRARY_H
+
+/*
+ *	Options agnostics libraries contents
+ */
+
+extern char *SelL_strdup( const char * );	/* May missing on some system */
+extern int hash( const char * );	/* calculates hash code of a string */
+
+
+#endif
--- src/SeleneLibrary/misc.c
+++ src/SeleneLibrary/misc.c
@@ -1,4 +1,4 @@
-#include "misc.h"
+#include "libSelene.h"
 
 #include <assert.h>
 #include <stdlib.h>
