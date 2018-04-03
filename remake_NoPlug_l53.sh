3ac61a11a044fe53bac99bcd2e941a2e5165c48b
destroyedlolo <l_faillie@yahoo.com>
2018-04-03 23:19:29 +0200

    Compile against Lua 5.3.4 in test directory

---
+++ remake_NoPlug_l53.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# this version compile against Lua 5.3.4
+
+# USE_MQTT - use Paho's MQTT layer
+# DEBUG - Add debuging messages
+
+# PLUGIN_DIR=/usr/local/lib/Selene
+PLUGIN_DIR=$( pwd )
+
+LUA_DIR=/home/laurent/Projets/lua-5.3.4/install
+
+echo "Don't forget"
+echo export LD_LIBRARY_PATH=$PLUGIN_DIR:$LD_LIBRARY_PATH
+
+
+
+cd src
+
+cd SeleneLibrary
+echo
+echo "Selene Library"
+echo "--------------"
+LFMakeMaker -v +f=Makefile --opts="-I$LUA_DIR/include -Wall -fPIC" *.c -so=../../libSelene.so > Makefile
+
+cd ..
+
+echo
+echo "Main source"
+echo "-----------"
+LFMakeMaker -v +f=Makefile --opts="-I$LUA_DIR/include -L$LUA_DIR/lib -Wall -DUSE_CURSES -DUSE_MQTT -DPLUGIN_DIR=$PLUGIN_DIR -L$PLUGIN_DIR -lSelene -DxDEBUG -lpaho-mqtt3c -llua -lm -ldl -Wl,--export-dynamic -lpthread" *.c -t=../Selene > Makefile
+
