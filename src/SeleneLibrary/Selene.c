114057858f55aa7179e336644880e0b1157c506f
destroyedlolo <l_faillie@yahoo.com>
2018-04-04 17:01:51 +0200

    Add Sleep()

--- src/SeleneLibrary/Selene.c
+++ src/SeleneLibrary/Selene.c
@@ -1,8 +1,19 @@
 /* Selene object */
 
 #include <unistd.h>             /* gethostname(), ... */
+#include <time.h>
 
 #include "libSelene.h"
+
+static int SelSleep( lua_State *L ){
+	struct timespec ts;
+	lua_Number lenght = luaL_checknumber(L, 1);
+	ts.tv_sec = (time_t)lenght;
+	ts.tv_nsec = (unsigned long int)((lenght - (time_t)lenght) * 1e9);
+
+	nanosleep( &ts, NULL );
+	return 0;
+}
 
 static int SelHostname( lua_State *L ){
         char n[HOST_NAME_MAX];
@@ -14,6 +25,7 @@ static int SelHostname( lua_State *L ){
 
 /* Selene own functions */
 static const struct luaL_Reg seleneLib[] = {
+	{"Sleep", SelSleep},
         {"Hostname", SelHostname},
         {NULL, NULL} /* End of definition */
 };
