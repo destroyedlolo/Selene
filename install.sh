d7bd2a8f0e480395546af49fd15b409d6258daee
destroyedlolo <l_faillie@yahoo.com>
2019-07-21 10:52:39 +0200

    Forgot the include file

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 
 mkdir -p /usr/local/lib/Selene || { echo "can't create target directory, please check if your user has enough rights" ; exit 0; }
 
+cp src/SeleneLibrary/libSelene.h /usr/local/include
 cp Sel*.so /usr/local/lib/Selene
 cp libSelene.so /usr/local/lib
 cp Selene /usr/local/bin
