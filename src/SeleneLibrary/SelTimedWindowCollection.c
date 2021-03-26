fb29f3c0be4ae0bc591d1499d46dcdc5fd908bc9
destroyedlolo <l_faillie@yahoo.com>
2021-03-26 01:44:52 +0100

    Improve comment

--- src/SeleneLibrary/SelTimedWindowCollection.c
+++ src/SeleneLibrary/SelTimedWindowCollection.c
@@ -327,7 +327,7 @@ static int stwcol_clear(lua_State *L){
 static int stwcol_create(lua_State *L){
 /* Create a new collection
  * -> 1: size of the collection
- * -> 2: seconds to be stored in records
+ * -> 2: seconds to be grouped in records
  */
 	struct SelTimedWindowCollection *col = malloc(sizeof(struct SelTimedWindowCollection));
 	struct SelTimedWindowCollection **p = (struct SelTimedWindowCollection **)lua_newuserdata(L, sizeof(struct SelTimedWindowCollection *));
