c10564188081906e10f2c25c6435c995888b4d54
destroyedlolo <l_faillie@yahoo.com>
2022-06-21 22:46:01 +0200

    Add more explicite title

--- gendoc.sh
+++ gendoc.sh
@@ -4,4 +4,4 @@
 echo "Selene own library"
 echo "------------------"
 
-ldoc -v --project "Selene library" --format markdown src/SeleneLibrary
+ldoc -v --project "Selene library" --title "Selene own library" --format markdown src/SeleneLibrary
