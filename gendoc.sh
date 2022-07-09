69f876cb227f94eb1e813fc5230bde9782ee8da4
destroyedlolo <l_faillie@yahoo.com>
2022-07-10 01:23:17 +0200

    Add DirectFB

--- gendoc.sh
+++ gendoc.sh
@@ -6,9 +6,15 @@ echo "------------------"
 
 ldoc -c docs/Selene.ld src/SeleneLibrary
 
-
+echo
 echo "Curses plug-in"
 echo "--------------"
 
 ldoc -c docs/Curses.ld src/SelPlugins/Curses
+
+echo
+echo "DirectFB plug-in"
+echo "----------------"
+
+ldoc -c docs/DirectFB.ld src/SelPlugins/DirectFB
 
