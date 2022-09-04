b2d4f4633d85fdd0b653437aba0b3e1af139f88a
destroyedlolo <l_faillie@yahoo.com>
2022-09-04 20:37:38 +0200

    Comment patterns

--- src/SelPlugins/DRMCairo/SelDCPattern.c
+++ src/SelPlugins/DRMCairo/SelDCPattern.c
@@ -1,6 +1,8 @@
-/* SelDCPattern
- *
- * This file contains all stuffs related to Cairo patterns
+/***
+ * Cairo patterns
+
+@classmod SelDCPattern
+
  *
  * 23/05/2020 LF : Creation
  */
@@ -17,42 +19,16 @@ cairo_pattern_t *checkSelDCPattern(lua_State *L, int where){
 	return (cairo_pattern_t *)*r;
 }
 
-static int Release(lua_State *L){
-	/* Delete a pattern
-	 * (i.e. remove all references to its objects)
-	 */
-	cairo_pattern_t *pat = checkSelDCPattern(L, 1);
-
-	cairo_pattern_destroy(pat);
-
-	return 0;
-}
-
-static int addFixPoint(lua_State *L){
-	/* Add a new point to fix point
-	 *	-> offset where to put this point (b/w 0.0 and 1.0)
-	 *	-> r,g,b 
-	 *	-> a opacity (optional, default 1)
-	 */
-	cairo_pattern_t *pat = checkSelDCPattern(L, 1);
-	lua_Number offset = luaL_checknumber(L, 2);
-	lua_Number r = luaL_checknumber(L, 3);
-	lua_Number g = luaL_checknumber(L, 4);
-	lua_Number b = luaL_checknumber(L, 5);
-	lua_Number a = 1.0;
-	if(lua_gettop(L) > 5)
-		a = luaL_checknumber(L, 6);
-
-	cairo_pattern_add_color_stop_rgba(pat, offset, r,g,b,a);
-
-	return 0;
-}
-
 static int createLinear(lua_State *L){
-	/* Create linear gradient
-	 *	-> x1,y1 : coordinate of the start point
-	 *	-> x2,y2 : coordinate of the end point
-	 *	<- new SelDCPattern object
+	/** Create linear gradient
+	 *
+	 * @function createLinear
+	 * @tparam Number x1 coordinate of the start point
+	 * @tparam Number y1 coordinate of the start point
+	 * @tparam Number x2 coordinate of the end point
+	 * @tparam Number y2 coordinate of the end point
+	 *
+	 * @treturn SelDCPattern pattern
 	 */
 	lua_Number x1 = luaL_checknumber(L, 1);
 	lua_Number y1 = luaL_checknumber(L, 2);
@@ -82,11 +58,15 @@ static int createLinear(lua_State *L){
 }
 
 static int createCircle(lua_State *L){
-	/* Create radial gradient
-	 * cx0,cy0 : center of the 1st circle
-	 * r0 : radius of the 1st circle
-	 * cx1,cy1 : center of the 1st circle
-	 * r1 : radius of the 1st circle
+	/** Create radial gradient
+	 *
+	 * @function createCircle
+	 * @tparam Number cx0 center of the 1st circle
+	 * @tparam Number cy0 center of the 1st circle
+	 * @tparam Number r0 radius of the 1st circle
+	 * @tparam Number cx1 center of the 2nd circle
+	 * @tparam Number cy1 center of the 2nd circle
+	 * @tparam Number r1 radius of the 2nd circle
 	 */
 	lua_Number cx0 = luaL_checknumber(L, 1);
 	lua_Number cy0 = luaL_checknumber(L, 2);
@@ -115,6 +95,50 @@ static int createCircle(lua_State *L){
 	}
 
 	return 1;
+}
+
+static int Release(lua_State *L){
+	/** Delete a pattern
+	 *
+	 * Remove all references to its objects.
+	 *
+	 * @function Release
+	 */
+	/** Delete a pattern
+	 *
+	 * Remove all references to its objects.
+	 *
+	 * @function destroy
+	 */
+	cairo_pattern_t *pat = checkSelDCPattern(L, 1);
+
+	cairo_pattern_destroy(pat);
+
+	return 0;
+}
+
+static int addFixPoint(lua_State *L){
+	/** Adds a translucent color stop to a gradient pattern
+	 *
+	 * @function addFixPoint
+	 * @tparam Number offset where to put this point (b/w 0.0 and 1.0)
+	 * @tparam Number r colors component
+	 * @tparam Number g colors component
+	 * @tparam Number b colors component
+	 * @tparam Number opacity optional, default 1
+	 */
+	cairo_pattern_t *pat = checkSelDCPattern(L, 1);
+	lua_Number offset = luaL_checknumber(L, 2);
+	lua_Number r = luaL_checknumber(L, 3);
+	lua_Number g = luaL_checknumber(L, 4);
+	lua_Number b = luaL_checknumber(L, 5);
+	lua_Number a = 1.0;
+	if(lua_gettop(L) > 5)
+		a = luaL_checknumber(L, 6);
+
+	cairo_pattern_add_color_stop_rgba(pat, offset, r,g,b,a);
+
+	return 0;
 }
 
 /* Object's own functions */
