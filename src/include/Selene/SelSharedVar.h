259befa4bcefd8b542f3279ea238bd64b4a95415
destroyedlolo <l_faillie@yahoo.com>
2024-03-06 00:30:08 +0100

    SharedVar on WIP

--- src/include/Selene/SelSharedVar.h
+++ src/include/Selene/SelSharedVar.h
@@ -8,18 +8,28 @@
 #ifndef SELSHAREDVAR_VERSION
 
 #include <Selene/libSelene.h>
-#include <Selene/SelLua.h>
 
 /* *********** 
  * /!\ CAUTION : BUMP THIS VERSION AT EVERY CHANGE INSIDE GLUE STRUCTURE
  * ***********/
 #define SELSHAREDVAR_VERSION 1
 
+	/* ***
+	 * Shared variables
+	 * ***/
+
+enum SharedObjType {
+	SOT_UNKNOWN = 0,	/* Invalid variable */
+	SOT_NUMBER,		/* Integers */
+	SOT_STRING,		/* Dynamically allocated string (managed by sharedobj' functions) */
+	SOT_XSTRING		/* Const char * managed externally (constant, allocated elsewhere ...) */
+};
+
 struct SelSharedVar {
 	struct SelModule module;
 
 		/* Call backs */
-
+	void (*setNumber)(double);
 };
 
 #endif
