2e9505c807752e9a833f52a93ac6982b9e7239ba
destroyedlolo <l_faillie@yahoo.com>
2024-09-08 15:51:33 +0200

    Add lowlevels

--- src/SelPlugins/LCD/SelLCD.c
+++ src/SelPlugins/LCD/SelLCD.c
@@ -1,5 +1,8 @@
 /***
  * Display messages on an LCD textual screen (like 1602 one)
+ *
+ * Based on https://fr.wikipedia.org/wiki/HD44780 
+ * and inspired by BitBank https://github.com/bitbank2/LCD1602
 
 @classmod SelOLED
 
@@ -10,16 +13,115 @@
 #include <Selene/SeleneCore.h>
 #include <Selene/SelLog.h>
 
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
 static struct SelLCD selLCD;
 
 static struct SeleneCore *selCore;
 static struct SelLog *selLog;
 static struct SelLua *selLua;
 
-struct LCDscreen {
-	int bus;		/* I2C bus file descriptor */
-	bool backlight;	/* is backlight enabled */
-};
+/* I2C expender bits usage :
+ *
+ * 0 : RS (0 = command, 1 = data)
+ * 1 : Read (0) / Write (1)
+ * 2 : Enable
+ * 3 : Backlight
+ */
+#define RS_CMD 0
+#define RS_DATA 0x01
+#define RW_R 0
+#define RW_W 0x02
+#define ENABLE 0x04
+#define BACKLIGHT 0x08
+
+static void SendQuarter(struct LCDscreen *screen, uint8_t b){
+/* Send the provided quarter */
+
+	write(screen->bus, &b, 1);	/* Present the data on the gpio */
+	usleep(selLCD.clock_pulse);
+
+	b |= ENABLE;		/* Rise 'E' */
+	write(screen->bus, &b, 1);
+	usleep(selLCD.clock_pulse);
+
+	b &= ~(ENABLE);		/* Lower 'E' */
+	write(screen->bus, &b, 1);
+	usleep(selLCD.clock_process);
+}
+
+static void lcdc_SendCmd(struct LCDscreen *screen, uint8_t dt){
+/** 
+ * @brief Send a command to the LCD controller
+ *
+ * @function SendCmd
+ * @tparam uint8_t command to send
+ */
+	uint8_t t = RS_CMD;	/* It's a Command */
+	t |= screen->backlight ? BACKLIGHT : 0;		/* Is the backlight on ? */
+
+		/* Most significant quarter first */
+	t |= dt & 0xf0;
+	SendQuarter(screen, t);
+
+	t &= 0x0f;	/* Keep only control bits */
+	t |= dt << 4;	/* send less significant quarter */
+	SendQuarter(screen, t);
+}
+
+static void lcdc_SendData(struct LCDscreen *screen, uint8_t dt){
+/** 
+ * @brief Send a data to the LCD controller
+ *
+ * @function SendData
+ * @tparam uint8_t data to send
+ */
+	uint8_t t = RS_DATA;	/* It's a Command */
+	t |= screen->backlight ? BACKLIGHT : 0;		/* Is the backlight on ? */
+
+		/* Most significant quarter first */
+	t |= dt & 0xf0;
+	SendQuarter(screen, t);
+
+	t &= 0x0f;	/* Keep only control bits */
+	t |= dt << 4;	/* send less significant quarter */
+	SendQuarter(screen, t);
+}
+
+static bool lcdc_Init(struct LCDscreen *screen, uint16_t bus_number, uint8_t address, bool twolines, bool y11){
+/** 
+ * @brief Initialize connection to the screen
+ *
+ * @function Init
+ * @param screen point to the screen handle
+ * @tparam uint16_t I2C bus number
+ * @tparam uint8_t Screen I2C address
+ * @tparam boolean true if the screen has 2 lines 
+ * @tparam boolean true if the screen is 11 pixel hight
+ * @treturn boolean false if we faced a technical error
+ */
+	char sbus[16];
+	sprintf(sbus, "/dev/i2c-%u", bus_number);
+
+	if((screen->bus = open(sbus, O_RDWR)) < 0)
+		return false;
+
+	if(ioctl(screen->bus, I2C_SLAVE, address) < 0)
+		return false;
+
+		/* Initializing 
+		 * SET + 4 bits mode
+		 * We're sending the upper quarter first so 0x02 is really 0x20.
+		 */
+	selLCD.SendCmd(screen, 0x02);
+
+		/* Now sending the full configuration */
+	selLCD.SendCmd(screen, 0x02 | (twolines ? 0x08 : 0) | (y11 ? 0x04 : 0));
+	
+	return true;
+}
 
 static const struct luaL_Reg LCDLib[] = {
 	{NULL, NULL}    /* End of definition */
@@ -57,6 +159,11 @@ bool InitModule( void ){
 
 	registerSelLCD(NULL);
 	selLua->AddStartupFunc(registerSelLCD);
+
+		/* Callbacks */
+	selLCD.Init = lcdc_Init;
+	selLCD.SendCmd = lcdc_SendCmd;
+	selLCD.SendData = lcdc_SendData;
 
 	return true;
 }
--- src/include/Selene/SelPlug-in/SelLCD.h
+++ src/include/Selene/SelPlug-in/SelLCD.h
@@ -10,15 +10,29 @@
 #include <Selene/libSelene.h>
 #include <Selene/SelLua.h>
 
+#include <unistd.h>
+
 /* *********** 
  * /!\ CAUTION : BUMP THIS VERSION AT EVERY CHANGE INSIDE GLUE STRUCTURE
  * ***********/
 #define SELLCD_VERSION 2
 
+struct LCDscreen {
+	int bus;		/* I2C bus file descriptor */
+	bool backlight;	/* is backlight enabled */
+};
+
 struct SelLCD {
 	struct SelModule module;
 
+	useconds_t clock_pulse;		/* 'E' clock */
+	useconds_t clock_process;	/* time to process */
+	
 		/* Call backs */
+	bool (*Init)(struct LCDscreen *, uint16_t bus_number, uint8_t address, bool twolines, bool y11);
+
+	void (*SendCmd)(struct LCDscreen *, uint8_t);
+	void (*SendData)(struct LCDscreen *, uint8_t);
 };
 
 #endif
